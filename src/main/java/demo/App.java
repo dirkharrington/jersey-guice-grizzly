389c80c5df53a2b5fce2335491f83fe1f35dfa04
Dirk Harrington <dirk@stackframe.com>
2014-01-08 22:36:22 -0500

    Fix compile error; Formatting

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -15,63 +15,64 @@ import com.google.inject.servlet.GuiceServletContextListener;
 import com.google.inject.servlet.ServletModule;
 import com.sun.grizzly.http.embed.GrizzlyWebServer;
 import com.sun.grizzly.http.servlet.ServletAdapter;
+import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
 
 public class App {
-	
-	@Path("/hello")
-	public static class Resource {
-		
-		@Inject Counter counter;
-			
-		@GET
-		public String get() {
-			return "Hello, User number " + counter.getNext();
-		}
-	}
-	
-	@Singleton
-	public static class Counter {
-		private final AtomicInteger counter = new AtomicInteger(0);
-		public int getNext() {
-			return counter.incrementAndGet();
-		}
-	}
+        
+        @Path("/hello")
+        public static class Resource {
+            
+            @Inject Counter counter;
+                    
+            @GET
+            public String get() {
+                return "Hello, User number " + counter.getNext();
+            }
+        }
+        
+        @Singleton
+        public static class Counter {
+            private final AtomicInteger counter = new AtomicInteger(0);
+            public int getNext() {
+                return counter.incrementAndGet();
+            }
+        }
 
-	public static class Config extends GuiceServletContextListener {
-		@Override
-		protected Injector getInjector() {
-			return Guice.createInjector(new ServletModule(){
-				@Override
-				protected void configureServlets() {
-                    // excplictly bind GuiceContainer before binding Jersey resources
-                    // otherwise resource won't be available for GuiceContainer
-                    // when using two-phased injection
-                    bind(GuiceContainer.class);
+        public static class Config extends GuiceServletContextListener {
+            @Override
+            protected Injector getInjector() {
+                return Guice.createInjector(new ServletModule(){
+                    @Override
+                    protected void configureServlets() {
+                        // excplictly bind GuiceContainer before binding Jersey resources
+                        // otherwise resource won't be available for GuiceContainer
+                        // when using two-phased injection
+                        bind(GuiceContainer.class);
 
-                    // bind Jersey resources
-                    PackagesResourceConfig resourceConfig = new PackagesResourceConfig("demo");
-                    for (Class<?> resource : resourceConfig.getClasses()) {
-                        bind(resource);
-                    }
+                        // bind Jersey resources
+                        PackagesResourceConfig resourceConfig = new PackagesResourceConfig("demo");
+                        for (Class<?> resource : resourceConfig.getClasses()) {
+                            bind(resource);
+                        }
 
-                    // Serve resources with Jerseys GuiceContainer
-                    serve("/*").with(GuiceContainer.class);
-				}		
-			});
-		}		
-	}
-	
-	@SuppressWarnings("serial")
-	public static class DummySevlet extends HttpServlet { }
-	
-	public static void main(String[] args) throws Exception {
-		int port = Integer.valueOf(System.getProperty("port"));
-		GrizzlyWebServer server = new GrizzlyWebServer(port);
-		ServletAdapter adapter = new ServletAdapter(new DummySevlet());
-		adapter.addServletListener(Config.class.getName());
-		adapter.addFilter(new GuiceFilter(), "GuiceFilter", null);
-		server.addGrizzlyAdapter(adapter, new String[]{ "/" });
-		server.start();
-	}
+                        // Serve resources with Jerseys GuiceContainer
+                        serve("/*").with(GuiceContainer.class);
+                    }               
+                });
+            }               
+        }
+        
+        @SuppressWarnings("serial")
+        public static class DummySevlet extends HttpServlet { }
+        
+        public static void main(String[] args) throws Exception {
+            int port = Integer.valueOf(System.getProperty("port"));
+            GrizzlyWebServer server = new GrizzlyWebServer(port);
+            ServletAdapter adapter = new ServletAdapter(new DummySevlet());
+            adapter.addServletListener(Config.class.getName());
+            adapter.addFilter(new GuiceFilter(), "GuiceFilter", null);
+            server.addGrizzlyAdapter(adapter, new String[]{ "/" });
+            server.start();
+        }
 }
